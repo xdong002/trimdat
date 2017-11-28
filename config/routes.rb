e95ca42cc8536ceac1e5f3797efa15ef4e089e09
Sherwin32 <sherwin.shann@gmail.com>
2017-11-28 15:18:50 -0800

    added route for about page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ get 'users/:id', to: 'users#show', as: 'user'
 get '/login', to: 'sessions#new'
 get '/logout', to: 'sessions#destroy'
 post '/sessions', to: 'sessions#create'
+get '/about', to: 'sessions#index', as: 'about'
+
 end
