44ee51fe004e9d2e82d2784f2ab649891d816457
marbisman <maria.arbisman@gmail.com>
2017-11-28 16:48:19 -0800

    updated models for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :documents, through: :document_librarys
 
   def self.confirm(params)
-    @user = User.find_by_id({email: params[:email]})
-    @user ? @user.authentication(params[:password]) : false
+    @user = User.find_by({user_name: params[:user_name]})
+    @user ? @user.authenticate(params[:password]) : false
   end
 end
