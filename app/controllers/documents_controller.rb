1bbcfe18c6b9a25265a4c70e0ccf32623ec2ff3e
marbisman <maria.arbisman@gmail.com>
2017-11-29 14:46:05 -0800

    updated content for about/footer and wrote method for first csv sort

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,26 @@
 class DocumentsController < ApplicationController
+  # array_of_lines = []
+
   def index
   end
 
   def new
-  end
+  #import the file as an array
+  # File.open("./FL_insurance_sample.csv", "r") do |f|
+  #   f.each_line do |line|
+  #   array_of_lines += line.split(/\t|\n|\r|\r\n/)
+  # end
 
   def show
   end
+
+  # def sort_by_first_value_number()
+  # #sort by first value, if number is the first value
+  # #converting to integers and comparing two items in the callback (sorting on them)
+  # array_of_lines! { |a, b| a[0].to_i <=> b[0].to_i }
+  # #modify the existing array
+  # array_of_lines.uniq!(&:first)
+  # array_of_lines.each { |line| p line }
+  # end
+
 end
